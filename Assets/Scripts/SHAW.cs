6e2615995b0292c251b8c5fe8de0c3333e73656f
grenbbychin <nic.wh.tan@gmail.com>
2026-04-29 11:40:07 +0800

    [Nic] buzzsaw
    A tiny buzzsaw that prints out various lines from hornet from hollow knight:silksong in the console on start.

---
+++ Assets/Scripts/SHAW.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SHAW : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("SHAW, HEGALE, ADINO, GARAMA, GITGUD");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
